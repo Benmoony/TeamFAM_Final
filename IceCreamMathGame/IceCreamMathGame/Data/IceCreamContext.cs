e239a4e419b2074643dbbb596a0b587d98eb557a
Benmoony <benmiller92@gmail.com>
2017-05-03 12:16:51 -0700

    Added missing line to DbInitalizer and changed context slightly

--- IceCreamMathGame/IceCreamMathGame/Data/IceCreamContext.cs
+++ IceCreamMathGame/IceCreamMathGame/Data/IceCreamContext.cs
@@ -11,7 +11,6 @@ namespace IceCreamMathGame.Data
     {
         public IceCreamContext(DbContextOptions<IceCreamContext> options) : base(options)
         {
-
         }
 
         public DbSet<Instructor> Instructors { get; set; }
