85b24b7c8fa32e9232ca0d13f2644ded4f759de5
Benmoony <benmiller92@gmail.com>
2017-05-15 11:45:51 -0700

    Removed bad migration and added a comment to where dependancy injection occurs

--- IceCreamMathGame/IceCreamMathGame/Startup.cs
+++ IceCreamMathGame/IceCreamMathGame/Startup.cs
@@ -27,6 +27,7 @@ namespace IceCreamMathGame
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // This is also the dependancy injection
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
